550281979fa3170e2fdf78dfcf0239627c733584
aleriza <aleriza.sultanova.on@gmail.com>
2023-07-08 20:06:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.a=7.5;
+        triangle.b=7.5;
+        triangle.c= 13.5;
+
+        triangle.area(triangle.a, triangle.b, triangle.c);
+    }
+}
